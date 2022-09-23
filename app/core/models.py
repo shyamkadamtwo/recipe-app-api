c6c65d023cee2fa68dfa34740da6fbdc85342099
shyamkadamtwo <shyamkadamtwo@gmail.com>
2022-09-23 17:42:28 +0530

    my commit

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,7 @@
 """
 Django models
 """
+from operator import truediv
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -14,8 +15,18 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user."""
+        if not email:
+            raise ValueError("User must have email address.")
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -32,3 +32,15 @@ class ModelTests(TestCase):
             user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
             
+    def test_new_user_without_email_raises_error(self):
+
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'sample123')
+
+    def test_create_superuser(self):
+
+        user = get_user_model().objects.create_superuser('ex@gmail.com','sample123')
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
+
